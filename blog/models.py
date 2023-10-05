dfd35ef4fedf67e2bd75dda4ba950db9345fc55e
Muhammaddiyor <muhammaddiyorashurov5@gmail.com>
2023-10-05 11:04:34 +0300

    add templates

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -24,3 +30,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month, self.publish.day, self.slug])
+
