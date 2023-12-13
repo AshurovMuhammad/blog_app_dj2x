f99d048c96eafa16c013bd15dd2f3f0431b6a283
Muhammaddiyor <muhammaddiyorashurov5@gmail.com>
2023-12-13 13:04:36 +0300

    Add db postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
